0022a127572768288e4ae5d4e35acc74bbf72fd8
briankunkel9 <briankunkel9@gmail.com>
2021-01-25 12:05:45 +0900

    editor class update

--- Editor/Editor.h
+++ Editor/Editor.h
@@ -8,6 +8,25 @@ public:
 
 	void Update(D3DXMATRIX & V, D3DXMATRIX & P);
 	void Render();
+
+public:
+	/*D3DXVECTOR2 ClickPosition()
+	{
+		D3DXVECTOR2 mouse = Mouse->Position();
+
+		mouse.x = mouse.x - (float)Width * 0.5f;
+		mouse.y = (mouse.y - (float)Height * 0.5f) * -1.0f;
+		
+		D3DXVECTOR2 camera = values->MainCamera->Position();
+		D3DXVECTOR2 position = mouse;
+
+		position.x *= (float)(horizontal.y - horizontal.x) / Width;
+		position.y *= (float)(vertical.y - vertical.x) / Height;
+
+		position += camera;
+
+		return position;
+	}*/
 	
 private:
 	SceneValues * values;
@@ -16,4 +35,22 @@ private:
 	int layers_n = 0;
 
 	int selected_layer = -1;
+
+	//Grid* grid;
+
+
+//private:
+//	class Object* clickedObject;
+//	D3DXVECTOR2 clickedStartClickedPosition;
+//	D3DXVECTOR2 markerStartPosition;
+//
+//private:
+//	Sprite* backGround;
+//
+//	class Player* player;
+//	vector<class Object*> objects;
+//
+//	// Mode to Draw Liner
+//	vector<class Marker*> markerToDrawLiner;
+//	vector<class Liner*> liners;
 };
